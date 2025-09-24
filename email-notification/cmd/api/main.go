ed9cae9fa0519c580cc870f1e46a63cfd15bb329
Guilherme <guiteixeira1926@gmail.com>
2025-09-24 12:56:59 -0300

    chore: add dockerfile for api module using multi-stage build technique. Add initial echo server structure.

--- email-notification/cmd/api/main.go
+++ email-notification/cmd/api/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/health-check", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK!")
+	})
+
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	log.Println("Starting API server on port", port)
+	if err := e.Start(port); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("shutting down the server: %v", err)
+	}
+}
