6cf7f6702c463d3d07f9362cd6c68dca24ef4518
Guilherme <guiteixeira1926@gmail.com>
2025-09-24 13:37:33 -0300

    chore: add dockerfile for worker module using multi-stage build technique.

--- email-notification/cmd/worker/main.go
+++ email-notification/cmd/worker/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	log.Println("Notification worker starting...")
+
+	for {
+		log.Println("Worker is alive and waiting for messages...")
+		time.Sleep(10 * time.Second)
+	}
+}
